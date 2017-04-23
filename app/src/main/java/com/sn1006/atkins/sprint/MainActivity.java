e72f1d6502efbdab3371e8f525f8be0d60e74559
jonathanjbrooks <jonathanjhbrooks@gmail.com>
2017-04-23 14:08:09 -0400

    test var

--- app/src/main/java/com/sn1006/atkins/sprint/MainActivity.java
+++ app/src/main/java/com/sn1006/atkins/sprint/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int mTestVar;
+    int jonTestVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
