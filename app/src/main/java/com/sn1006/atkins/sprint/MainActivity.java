a1765bf1a2e8112ee747af4e7cdf86cd48655adf
Atkins <atkinskevin2@gmail.com>
2017-04-23 13:46:51 -0400

    Initial commit of project

---
+++ app/src/main/java/com/sn1006/atkins/sprint/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sn1006.atkins.sprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
