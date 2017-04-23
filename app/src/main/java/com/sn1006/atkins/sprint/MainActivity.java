50c7191bfd6cb15701986120a8d9f69de9783539
Atkins <atkinskevin2@gmail.com>
2017-04-23 13:54:20 -0400

    Initial commit of project

--- app/src/main/java/com/sn1006/atkins/sprint/MainActivity.java
+++ app/src/main/java/com/sn1006/atkins/sprint/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int mTestVar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
