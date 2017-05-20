e45fbf9b5987135cf001ef24b4bd7f6000f75926
Atkins <atkinskevin2@gmail.com>
2017-05-20 10:45:34 -0400

    Introduced app activity structure

---
+++ app/src/main/java/com/sn1006/atkins/sprint/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.sn1006.atkins.sprint;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
