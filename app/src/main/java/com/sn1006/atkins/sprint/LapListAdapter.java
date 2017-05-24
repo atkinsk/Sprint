032e2fdeff99370737c5fa45bb5781ec9f9a2f00
Atkins <atkinskevin2@gmail.com>
2017-05-24 19:01:55 -0400

    Fixed formatting issue for lap times

--- app/src/main/java/com/sn1006/atkins/sprint/LapListAdapter.java
+++ app/src/main/java/com/sn1006/atkins/sprint/LapListAdapter.java
@@ -1,7 +1,6 @@
 package com.sn1006.atkins.sprint;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
